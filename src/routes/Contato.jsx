695b50402115391a3176b139d7d5b6c9fe1a73dd
Jorge Booz <107008455+jorgebooz@users.noreply.github.com>
2024-05-07 21:40:48 -0300

    feat: contato

--- src/routes/Contato.jsx
+++ src/routes/Contato.jsx
@@ -1,19 +1,37 @@
 import {} from 'react';
 import '../css/estilo.css';
+import styles from '../css/contato.module.css'
 
 function Contato() {
   return (
     <>
-      <section className="contato">
-    
-        <form>
-        <h1>Contato</h1>
-          <p>
-          <input type="text" placeholder='Digite o Assunto'/>
-          </p>
-          <textarea placeholder='Deixe sua mensagem'></textarea>
-        </form>
-      </section>
+    <div className={`${styles.container} ${styles.bgImage}`}>
+      <div className={styles.main}>
+          <div className={`${styles.contatoTop} ${styles.azul}`}>
+              <h1>PORTAL</h1>
+              <h1>DO CLIENTE</h1>
+              <p>Como podemos <a>te ajudar?</a></p>
+          </div>
+              
+              <div className={styles.contatoCard}>
+                  <p className={styles.titulo}>MEUS PEDIDOS</p>
+                  <p className={styles.cardText}><a class="azul">ᐳ</a>Acompanhar meus pedidos</p>
+                  <p className={styles.cardText}><a class="azul">ᐳ</a>Histórico de pedidos</p>
+              </div>
+              <div className={styles.contatoCard}>
+                  <p className={styles.titulo}>TROCAS & CANCELAMENTO</p>
+                  <p className={styles.cardText}><a class="azul">ᐳ</a>Cancelar um pedido</p>
+                  <p className={styles.cardText}><a class="azul">ᐳ</a>Trocar um pedido</p>
+              </div>
+              <div className={styles.contatoCard}>
+                  <p className={styles.titulo}>MEUS DADOS</p>
+                  <p className={styles.cardText}><a class="azul">ᐳ</a>Cancelar um pedido</p>
+                  <p className={styles.cardText}><a class="azul">ᐳ</a>Trocar um pedido</p>
+              </div>
+      </div>
+      
+    </div>
+      
     </>
   );
 }
